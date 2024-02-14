b4ca4682f3ef458cd2f92d3e067d16ca7060e297
Crowdbotics <engineering@crowdbotics.com>
2024-02-14 20:40:41 +0000

    Initial commit

---
+++ backend/sample_3_etsy_clon_133455/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for sample_3_etsy_clon_133455 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "sample_3_etsy_clon_133455.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sample-3-etsy-clon-133455",
+  url: "https://sample-3-etsy-clon-133455.botics.co",
+  api: "https://sample-3-etsy-clon-133455.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
