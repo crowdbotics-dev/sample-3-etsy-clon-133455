354077709892daafab89c0c061e4672e7de36232
Aline Tavares <aline@crowdbotics.com>
2024-02-21 10:09:43 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "sample-3-etsy-clon-133455",
-  url: "https://sample-3-etsy-clon-133455.botics.co",
-  api: "https://sample-3-etsy-clon-133455.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "sample-3-etsy-clon-133455",
+  "url": "https://sample-3-etsy-clon-133455.botics.co",
+  "api": "https://sample-3-etsy-clon-133455.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "LoginRegister"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
